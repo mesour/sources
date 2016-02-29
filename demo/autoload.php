06e3a1db6fa95234831b116028fbb3e7d49e76e7
mesour <matous.nemec@gmail.com>
2016-02-29 13:59:41 +0100

    Added files for backward compatibility

---
+++ demo/autoload.php
@@ -0,0 +1,7 @@
+<?php
+
+define('SRC_DIR', __DIR__ . '/../src/');
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+\Tracy\Debugger::enable(\Tracy\Debugger::DEVELOPMENT, __DIR__ . '/log');
---
+++ demo/bootstrap.php
@@ -0,0 +1,39 @@
+<?php
+
+if(!defined('DISABLE_AUTOLOAD')) {
+    require_once 'autoload.php';
+}
+
+// bootstrap.php
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Common\Annotations\AnnotationReader;
+
+$paths = array(__DIR__ . "/../tests/Entity");
+
+// Create a simple "default" Doctrine ORM configuration for Annotations
+$isDevMode = true;
+
+$config = Setup::createConfiguration($isDevMode);
+
+$driver = new \Doctrine\ORM\Mapping\Driver\AnnotationDriver(new AnnotationReader(), $paths);
+\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader('class_exists');
+$config->setMetadataDriverImpl($driver);
+
+// or if you prefer yaml or XML
+//$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
+//$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
+
+// database configuration parameters
+if(!isset($conn)) {
+    $conn = array(
+        'driver'   => 'mysqli',
+        'host'     => '127.0.0.1',
+        'user'     => 'root',
+        'password' => 'root',
+        'dbname'   => 'sources_test',
+    );
+}
+
+// obtaining the entity manager
+return EntityManager::create($conn, $config);
